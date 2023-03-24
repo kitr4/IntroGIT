ace40b0aba351cbe38be4257d223d1559b78ba1a
kitr4 <128846671+kitr4@users.noreply.github.com>
2023-03-24 20:30:09 +0100

    opstart projektInitial commit

---
+++ NytProjektGitHUB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NytProjektGitHUB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
